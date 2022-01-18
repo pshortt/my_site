b8b22e3138642e1509e4e476c678ac09615fecd8
pat <pshortt21@gmail.com>
2022-01-18 00:46:32 -0500

    initialize polls app

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("my_site.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("polls/", include("polls.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
